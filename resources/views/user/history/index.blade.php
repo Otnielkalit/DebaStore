5e8453e0a969f28fbab39dc9440f6ed51a0ae3b3
Gilbert Marpaung <gilbertmarpaung0@gmail.com>
2022-06-18 03:31:45 +0700

    CRUD MENU

--- resources/views/user/history/index.blade.php
+++ resources/views/user/history/index.blade.php
@@ -42,7 +42,7 @@
                                     </td>
                                     <td>Rp. {{ number_format($pesanan->jumlah_harga+$pesanan->kode )}}</td>
                                     <td>
-                                        <a href="{{ url('history') }}\{{ $pesanan->id }}" class="btn btn-primary"><i class="fa fa-info"></i>Detail</a>
+                                        <a href="{{ url('history') }}\{{ $pesanan->id }}" class="btn btn-primary">Detail</a>
                                     </td>
                                 </tr>
                             @endforeach
